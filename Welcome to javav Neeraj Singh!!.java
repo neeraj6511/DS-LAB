710702c2bbcfce1ab97de133cf188e1cf8189c6f
neeraj6511 <101538685+neeraj6511@users.noreply.github.com>
2022-03-14 23:39:06 +0530

    Welcome to javav Neeraj Singh!! 14/03/2022

---
+++ Welcome to javav Neeraj Singh!!.java
@@ -0,0 +1,10 @@
+
+public class WelcomeToJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to javav Neeraj Singh!!");
+
+	}
+
+}
